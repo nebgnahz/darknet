1355cfd5a3a168baf2b76ddb265dd1565c64d95c
Ben Zhang <benzh@cs.berkeley.edu>
2017-02-24 23:43:37 -0800

    feat: iterate results and print values

--- src/wrapper.c
+++ src/wrapper.c
@@ -37,8 +37,6 @@ void detect_api(char *filename) {
   int j;
   float nms = .4;
 
-  image im = load_image_color(filename, 0, 0);
-  image sized = resize_image(im, net.w, net.h);
   layer l = net.layers[net.n - 1];
 
   box *boxes = calloc(l.w * l.h * l.n, sizeof(box));
@@ -46,9 +44,12 @@ void detect_api(char *filename) {
   for (j = 0; j < l.w * l.h * l.n; ++j)
     probs[j] = calloc(l.classes + 1, sizeof(float *));
 
-  float *X = sized.data;
   float thresh = 0.24;
   float hier_thresh = .5;
+
+  image im = load_image_color(filename, 0, 0);
+  image sized = resize_image(im, net.w, net.h);
+  float *X = sized.data;
 
   time = clock();
   network_predict(net, X);
@@ -58,10 +59,24 @@ void detect_api(char *filename) {
     do_nms_obj(boxes, probs, l.w * l.h * l.n, l.classes, nms);
   else if (nms)
     do_nms_sort(boxes, probs, l.w * l.h * l.n, l.classes, nms);
-  draw_detections(im, l.w * l.h * l.n, thresh, boxes, probs, names, alphabet,
-                  l.classes);
+
+  // The network will return l.w * l.h * l.n detections
+  // But only those exceeds the threshold
+  ///
+  for (int i = 0; i < l.w * l.h * l.n; ++i) {
+    int class = max_index(probs[i], l.classes);
+    float prob = probs[i][class];
+    if (prob > thresh) {
+      printf("%s: %.0f%%\n", names[class], prob * 100);
+      box b = boxes[i];
+      printf("%.2f %.2f %.2f %.2f\n", b.x, b.y, b.w, b.h);
+    }
+  }
+
+  //  draw_detections(im, l.w * l.h * l.n, thresh, boxes, probs, names,
+  //  alphabet, l.classes);
   save_image(im, "predictions");
-  show_image(im, "predictions");
+  // show_image(im, "predictions");
 
   free_image(im);
   free_image(sized);
